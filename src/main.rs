eebe61cd5941a90067f8c3ab0550ec6510f97d0a
Ambrosini Nico (IT-PTR-CEN1-BDE4) <nico.ambrosini@sbb.ch>
2023-03-16 15:48:53 +0100

    fix: changed listener address

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
+    let listener = TcpListener::bind("0.0.0.0:8000")
         .expect("Failed to bind to address");
     run(listener)?.await
 }
